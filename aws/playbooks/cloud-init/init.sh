572cc81c1e37fdd5f280160b1c281f3abace0319
root <root@ip-10-0-38-21.us-west-2.compute.internal>
2017-07-28 23:05:32 +0000

    Adding playbooks for openshift cloud

---
+++ aws/playbooks/cloud-init/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Enable root login
+sudo -u root bash << EOF
+sed -i -e 's/#PermitRootLogin yes/PermitRootLogin yes/g' -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+service sshd restart
+
+# Set root password
+echo "root:c0ntrail123" | chpasswd
+
+# Install Packages
+yum install git epel-release vim NetworkManager -y
+systemctl enable NetworkManager
+yum update -y
