4f862a2e7aacabb1711ece82c18f502a9b98d4bf
Savithru Lokanath <savithruml@users.noreply.github.com>
2017-11-06 12:11:51 -0800

    Update init.sh

--- aws/playbooks/cloud-init/init.sh
+++ aws/playbooks/cloud-init/init.sh
@@ -2,7 +2,7 @@
 
 # Enable root login
 sudo -u root bash << EOF
-sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i -e 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service sshd restart
 
 # Set root password
